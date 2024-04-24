3c1c779d129d9db5192ebb16916ef88da7f98705
Sharad <sharad-external@esper.io>
2024-04-24 09:53:46 +0530

    Completed the task to send the mail

--- test_google.py
+++ test_google.py
@@ -27,16 +27,23 @@ def test_login_Outlook(driver, getValue, excel_data):
     driver.implicitly_wait(2)
     driver.find_element_by_xpath(locators["HomePage"]["InputEmailBox"]%"Email").send_keys("sharadkumar@rtctek.com")
     driver.find_element_by_xpath(locators["HomePage"]["NextButton"]).click()
-    driver.find_element_by_xpath(locators["HomePage"]["InputEmailBox"]%"Password").send_keys("Sharad")
+    driver.find_element_by_xpath(locators["HomePage"]["InputEmailBox"]%"Password").send_keys("Ashi@990")
     driver.find_element_by_xpath(locators["HomePage"]["NextButton"]).click()
     driver.find_element_by_xpath(locators["HomePage"]["NextButton"]).click()
-
-    for i in range(len(excel_data)-1):
-        driver.find_element_by_xpath("//input[@id = 'topSearchInput']").click
-        driver.find_element_by_xpath("//input[@id = 'topSearchInput']").send_keys(excel_data)
-        print(excel_data[i].__str__)
-        time.sleep(150)
-    # input.
+    time.sleep(5)
+    driver.find_element_by_xpath(locators["HomePage"]["NewMail"]).click()
+    driver.find_element_by_xpath(locators["HomePage"]["MailTo"]).send_keys("sharadKumar@rtctek.com")
+    time.sleep(5)
+    driver.find_element_by_xpath(locators["HomePage"]["MailTo"]).send_keys(Keys.ENTER)
+    driver.find_element_by_xpath(locators["HomePage"]["MailSubject"]).click()
+    driver.find_element_by_xpath(locators["HomePage"]["MailSubject"]).send_keys(456)
+    time.sleep(3)
+    driver.find_element_by_xpath(locators["HomePage"]["MailTextBox"]).click()
+    # for i in range(len(excel_data)-1):
+    driver.find_element_by_xpath(locators["HomePage"]["MailTextBox"]).send_keys(excel_data)
+        # print(excel_data[i].__str__)
+    driver.find_element_by_xpath(locators["HomePage"]["MailSendButton"]).click()
+    time.sleep(5)
 
 # def test_Create_DSR(driver, excel_data):
     # data = excel_data()
