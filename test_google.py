40b7451e02e87833ddd8a2a906f478db8071cbd6
Sharad <sharad-external@esper.io>
2024-04-24 10:06:45 +0530

    Fixing the scrolling window issue

--- test_google.py
+++ test_google.py
@@ -42,6 +42,7 @@ def test_login_Outlook(driver, getValue, excel_data):
     # for i in range(len(excel_data)-1):
     driver.find_element_by_xpath(locators["HomePage"]["MailTextBox"]).send_keys(excel_data)
         # print(excel_data[i].__str__)
+    driver.execute_script("scroll(0, -250);")
     driver.find_element_by_xpath(locators["HomePage"]["MailSendButton"]).click()
     time.sleep(5)
 
